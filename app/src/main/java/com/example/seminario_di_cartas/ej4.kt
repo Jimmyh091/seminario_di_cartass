04288f047983da4e8a43ff7f98702f264a613a49
Jimmyh091 <jaimecaballero164@gmail.com>
2024-11-12 17:48:52 +0100

    lolaso

---
+++ app/src/main/java/com/example/seminario_di_cartas/ej4.kt
@@ -0,0 +1,35 @@
+package com.example.seminario_di_cartas
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.seminario_di_cartas.databinding.ActivityEj4Binding
+
+class ej4 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEj4Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityEj4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_ej4)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
