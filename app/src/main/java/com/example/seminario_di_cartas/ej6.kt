16c621553b2cd71fd6672a09415cf07ff0712abd
Jimmyh091 <jaimecaballero164@gmail.com>
2024-11-13 23:50:52 +0100

    hola : )

---
+++ app/src/main/java/com/example/seminario_di_cartas/ej6.kt
@@ -0,0 +1,48 @@
+package com.example.seminario_di_cartas
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
+
+class ej6 : AppCompatActivity() {
+
+    private lateinit var tiet : TextInputEditText
+    private lateinit var botonPasarDatos : Button
+
+    private lateinit var texto1 : TextView
+    private var num = (1..10).random()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.v("hola","sajfldask")
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ej6)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Log.v("asjdfkl", "jlasdk")
+        texto1 = findViewById<TextView>(R.id.ej6_tv)
+        texto1.text = "Numero que vamos a pasar: $num"
+
+        tiet = findViewById<TextInputEditText>(R.id.ej6_tiet)
+        botonPasarDatos = findViewById<Button>(R.id.ej6_b)
+
+        botonPasarDatos.setOnClickListener {
+            var intent = Intent(this, ej6Objetivo::class.java)
+            intent.putExtra("num", num.toInt())
+            intent.putExtra("input", tiet.text.toString())
+            startActivity(intent)
+        }
+        Log.v("asjdfkl", "jlassadfasdfasdfasdfasdfdk")
+    }
+}
